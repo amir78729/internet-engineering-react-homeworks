2644428984d22a8b99553943f0b40416fba19945
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-01-06 22:34:28 +0330

    adding eslint:))

--- src/components/Question1/Question1.js
+++ src/components/Question1/Question1.js
@@ -1,20 +1,48 @@
 import s from "./Question1.module.css"
 export const Question1Component = () => {
     return (
-        <div className={s.Q1}>
+        <div>
             <h1>سوال ۱</h1>
-            <p>
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-                در مورد virtual DOM میتوان گفت
-            </p>
+            <div className={s.Q1}>
+                <p>
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفتت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفتت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفتت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                    در مورد virtual DOM میتوان گفت
+                </p>
+            </div>
         </div>
     )
 }
